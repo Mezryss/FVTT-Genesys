7a762f5d87001feda7a4086c3549aee666b9dfea
Mezryss <mezryss@minalien.dev>
2023-02-24 20:27:51 -0800

    Add gulp option to zip dist/ folder into a release file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
+import fs from 'fs';
 import gulp from 'gulp';
-const {dest, series, src} = gulp;
+const { dest, series, src } = gulp;
 
 import gulpClean from 'gulp-clean';
 import gulpYaml from 'gulp-yaml';
+import gulpZip from 'gulp-zip';
+
+export function zip() {
+	let version;
+	try {
+		const systemJsonFile = fs.readFileSync('dist/system.json', { encoding: 'utf8' });
+		const systemJson = JSON.parse(systemJsonFile);
+		version = systemJson['version'];
+	} catch {
+		version = 'unknown';
+	}
+
+	return src('dist/**/*')
+		.pipe(gulpZip(`genesys-${version}.zip`))
+		.pipe(dest('.'));
+}
 
 export function clean() {
-	return src([
-		'public/lang/',
-		'public/system.json',
-		'public/template.json',
-	], {allowEmpty: true})
-		.pipe(gulpClean());
+	return src(['public/lang/', 'public/system.json', 'public/template.json'], { allowEmpty: true }).pipe(gulpClean());
 }
 
 export function data() {
-	return src('yaml/**/*.yml')
-		.pipe(gulpYaml())
-		.pipe(dest('public/'));
+	return src('yaml/**/*.yml').pipe(gulpYaml()).pipe(dest('public/'));
 }
 
 function watchDirs() {
