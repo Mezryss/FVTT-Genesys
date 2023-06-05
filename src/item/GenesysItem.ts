4f0e224176d9831926cf8aea8c3345e58fd9f79e
Assembling Kings <assembling.kings@outlook.com>
2023-06-05 11:49:34 -0400

    Categorize Item and Actor types with optgroups
    Implements #11

--- src/item/GenesysItem.ts
+++ src/item/GenesysItem.ts
@@ -21,9 +21,27 @@ export default class GenesysItem<ItemDataModel extends foundry.abstract.DataMode
 		return <ItemDataModel>this.system;
 	}
 
+	/**
+	 * Override the _preCreate callback to call preCreate from the data model class, if present.
+	 * @inheritDoc
+	 */
 	protected override async _preCreate(data: PreDocumentId<this['_source']>, options: DocumentModificationContext<this>, user: foundry.documents.BaseUser) {
 		await (<IHasPreCreate<this>>this.systemData).preCreate?.(this, data, options, user);
 
 		return super._preCreate(data, options, user);
+	}
+
+	/**
+     * Override the createDialog callback to include an unique class that identifies the created dialog.
+     * @inheritDoc
+     */
+	static override createDialog(data?: { folder?: string | undefined; } | undefined, options?: Partial<FormApplicationOptions> | undefined): Promise<ClientDocument<foundry.documents.BaseItem> | undefined> {
+		// The 'dialog' class needs to be added explicitly, otherwise it won't be added by the super call.
+		const touchedOptions = {
+			...options,
+			classes: [...(options?.classes ?? []), 'dialog', 'dialog-item-create']
+		};
+
+		return super.createDialog(data, touchedOptions);
 	}
 }
