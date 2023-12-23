85bcc2a5ade9a0707eb85212309f757346003c67
Assembling Kings <assembling.kings@outlook.com>
2023-12-23 15:53:45 -0500

    Expand the info on the dice classes

--- src/dice/types/AbilityDie.ts
+++ src/dice/types/AbilityDie.ts
@@ -6,12 +6,15 @@
  * @file Definition for the Ability die.
  */
 
-import GenesysDie from '@/dice/types/GenesysDie';
+import GenesysDie, { DieCategory } from '@/dice/types/GenesysDie';
 
 /**
  * Ability (green) Die
  */
 export default class AbilityDie extends GenesysDie {
 	static override DENOMINATION = 'a';
+	static override GLYPH = 'A';
+	static override FORMULA = 'da';
+	static override CATEGORY = 'positive' as DieCategory;
 	static override FACES = [' ', 's', 's', 'ss', 'a', 'a', 'sa', 'aa'];
 }
--- src/dice/types/BoostDie.ts
+++ src/dice/types/BoostDie.ts
@@ -6,12 +6,15 @@
  * @file Definition for the Boost die.
  */
 
-import GenesysDie from '@/dice/types/GenesysDie';
+import GenesysDie, { DieCategory } from '@/dice/types/GenesysDie';
 
 /**
  * Boost (blue) Die
  */
 export default class BoostDie extends GenesysDie {
 	static override DENOMINATION = 'b';
+	static override GLYPH = 'B';
+	static override FORMULA = 'db';
+	static override CATEGORY = 'positive' as DieCategory;
 	static override FACES = [' ', ' ', 's', 'sa', 'aa', 'a'];
 }
