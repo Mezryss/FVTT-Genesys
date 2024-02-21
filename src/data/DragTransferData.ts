9dc73f7598dda89fe3882315b3f33d507309c481
Assembling Kings <assembling.kings@outlook.com>
2024-02-20 21:08:14 -0500

    Fix re-rendering for Combat tabs (#144)
    Fixes #142. Also fixes drag & drop highlights for vehicles.

--- src/data/DragTransferData.ts
+++ src/data/DragTransferData.ts
@@ -21,9 +21,19 @@ export type CrewDragTransferData = DragTransferData &
 	};
 
 const GENESYS_DRAG_TYPE_PREFIX = 'genesys';
+const UPPER_CASE_PATTERN = /([A-Z]+)/g;
+const ENCODED_UPPER_CASE_PATTERN = /\|(\d+)\|/g;
+
+function encodeUpperCase(text: string) {
+	return text.replace(UPPER_CASE_PATTERN, (_match, group1: string) => `|${group1.codePointAt(0)}|`);
+}
+
+function decodeUpperCase(text: string) {
+	return text.replace(ENCODED_UPPER_CASE_PATTERN, (_match, group1: string) => String.fromCodePoint(parseInt(group1, 10)));
+}
 
 export function constructDragTransferTypeFromData(genesysType: string, uuid: ItemUUID | ActorUUID | TokenDocumentUUID) {
-	return `${GENESYS_DRAG_TYPE_PREFIX}/${genesysType}/${uuid}`;
+	return `${GENESYS_DRAG_TYPE_PREFIX}/${encodeUpperCase(genesysType)}/${encodeUpperCase(uuid)}`;
 }
 
 export function extractDataFromDragTransferTypes(dragTransferTypes?: readonly string[]) {
@@ -31,7 +41,7 @@ export function extractDataFromDragTransferTypes(dragTransferTypes?: readonly st
 		const genesysDragType = dragTransferTypes.find((transferType) => transferType.indexOf(GENESYS_DRAG_TYPE_PREFIX) === 0);
 
 		if (genesysDragType) {
-			const genesysDataSections = genesysDragType.split('/');
+			const genesysDataSections = decodeUpperCase(genesysDragType).split('/');
 
 			if (genesysDataSections.length === 3) {
 				return {
