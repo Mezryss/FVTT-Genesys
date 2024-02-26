bca76f35fc762ec565ff3721f563f90ef57110c9
Assembling Kings <assembling.kings@outlook.com>
2024-02-25 20:34:38 -0500

    Migrate vehicles with repeated actors

--- src/migrations/1-use-uuid-for-vehicle.ts
+++ src/migrations/1-use-uuid-for-vehicle.ts
@@ -1,94 +1,84 @@
 import GenesysActor from '@/actor/GenesysActor';
 import VehicleDataModel from '@/actor/data/VehicleDataModel';
-import { NAMESPACE as SETTINGS_NAMESPACE } from '@/settings';
-import { KEY_MIGRATION_VERSION } from '@/settings/alpha';
+import { MigrationStatus } from '@/migrations/MigrationHelper';
 
+/**
+ * Migration script for vehicles that are using an actor's id instead of the actor's uuid. The former is not completly unique
+ * and limits the type of actors it can refer to.
+ */
 export async function migrate_UseUuidForVehicles() {
-	const migrationVersion = game.settings.get<number>(SETTINGS_NAMESPACE, KEY_MIGRATION_VERSION) ?? 0;
-
-	if (migrationVersion === 0) {
-		const isGmHub = game.users.activeGM?.isSelf ?? (game.user.isGM && game.users.filter((user) => user.isGM && user.active).every((candidate) => candidate.id >= game.user.id));
-		if (!isGmHub) {
-			ui.notifications.warn('MIGRATION NEEDED: A migration related to the vehicles data is needed. Please wait for a GM to perform it before using the system.', { permanent: true });
-			return;
-		}
-
-		ui.notifications.warn('MIGRATION STARTED: Migrating data related to vehicles; please be patient and do not close the application.', { permanent: true });
-
-		// Get all the vehicles on the world and compendiums.
-		const vehiclesInWorld = game.actors.filter<GenesysActor<VehicleDataModel>>((actor) => actor.type === 'vehicle');
-		const vehiclesInCompendium = await Promise.all(
-			game.packs.reduce(
-				(accum, pack) => {
-					if (pack.metadata.type === 'Actor') {
-						pack.index.forEach((compendiumIndex) => {
-							if (compendiumIndex.type === 'vehicle') {
-								accum.push(pack.getDocument(compendiumIndex._id) as Promise<GenesysActor<VehicleDataModel>>);
-							}
-						});
-					}
-					return accum;
-				},
-				[] as Promise<GenesysActor<VehicleDataModel>>[],
-			),
-		);
-		const vehicles = vehiclesInWorld.concat(vehiclesInCompendium);
-
-		for (const vehicle of vehicles) {
-			const updateMap = new Map<string, any>();
-			const passengersList = vehicle.systemData.passengers.list;
-
-			// If the vehicle has passangers on them then we need to update their references from id's to UUID's.
-			if (passengersList.length > 0) {
-				const passengers: typeof passengersList = [];
-
-				for (const passenger of passengersList) {
-					const passengerActor = game.actors.get(passenger.uuid);
-
-					if (passengerActor) {
-						passengers.push({
-							uuid: passengerActor.uuid,
-							sort: passenger.sort,
-						});
-					}
-				}
-
-				updateMap.set('system.passengers.list', passengers);
-			}
-
-			let updateRoles = false;
-			const roles = [...vehicle.systemData.roles];
-			for (const role of roles) {
-				const roleMembers = role.members;
-
-				// If the role has any members then we need to update their references from id's to UUID's.
-				if (roleMembers.length > 0) {
-					const members: string[] = [];
-
-					for (const member of roleMembers) {
-						const memberActor = game.actors.get(member);
-
-						if (memberActor) {
-							members.push(memberActor.uuid);
+	// Get all the vehicles on the world and compendiums.
+	const vehiclesInWorld = game.actors.filter<GenesysActor<VehicleDataModel>>((actor) => actor.type === 'vehicle');
+	const vehiclesInCompendium = await Promise.all(
+		game.packs.reduce(
+			(accum, pack) => {
+				if (pack.metadata.type === 'Actor') {
+					pack.index.forEach((compendiumIndex) => {
+						if (compendiumIndex.type === 'vehicle') {
+							accum.push(pack.getDocument(compendiumIndex._id) as Promise<GenesysActor<VehicleDataModel>>);
 						}
-					}
+					});
+				}
+				return accum;
+			},
+			[] as Promise<GenesysActor<VehicleDataModel>>[],
+		),
+	);
+	const vehicles = vehiclesInWorld.concat(vehiclesInCompendium);
 
-					role.members = members;
-					updateRoles = true;
+	for (const vehicle of vehicles) {
+		const updateMap = new Map<string, any>();
+		const passengersList = vehicle.systemData.passengers.list;
+
+		// If the vehicle has passangers on them then we need to update their references from id's to UUID's.
+		if (passengersList.length > 0) {
+			const passengers: typeof passengersList = [];
+
+			for (const passenger of passengersList) {
+				const passengerActor = game.actors.get(passenger.uuid);
+
+				if (passengerActor) {
+					passengers.push({
+						uuid: passengerActor.uuid,
+						sort: passenger.sort,
+					});
 				}
 			}
 
-			// Update the roles array if at least one of them had to be updated.
-			if (updateRoles) {
-				updateMap.set('system.roles', roles);
-			}
+			updateMap.set('system.passengers.list', passengers);
+		}
 
-			if (updateMap.size > 0) {
-				await vehicle.update(Object.fromEntries(updateMap));
+		let updateRoles = false;
+		const roles = [...vehicle.systemData.roles];
+		for (const role of roles) {
+			const roleMembers = role.members;
+
+			// If the role has any members then we need to update their references from id's to UUID's.
+			if (roleMembers.length > 0) {
+				const members: string[] = [];
+
+				for (const member of roleMembers) {
+					const memberActor = game.actors.get(member);
+
+					if (memberActor) {
+						members.push(memberActor.uuid);
+					}
+				}
+
+				role.members = members;
+				updateRoles = true;
 			}
 		}
 
-		ui.notifications.warn('MIGRATION FINISHED: The vehicles data has been migrated, thanks for your patience.', { permanent: true });
-		await game.settings.set(SETTINGS_NAMESPACE, KEY_MIGRATION_VERSION, 1);
+		// Update the roles array if at least one of them had to be updated.
+		if (updateRoles) {
+			updateMap.set('system.roles', roles);
+		}
+
+		if (updateMap.size > 0) {
+			await vehicle.update(Object.fromEntries(updateMap));
+		}
 	}
+
+	return MigrationStatus.SUCCESS;
 }
---
+++ src/migrations/MigrationHelper.ts
@@ -0,0 +1,62 @@
+import { NAMESPACE } from '@/settings';
+import { KEY_MIGRATION_VERSION } from '@/settings/alpha';
+import { migrate_UseUuidForVehicles } from '@/migrations/1-use-uuid-for-vehicle';
+import { migrate_ActorOnlyOnceInCrew } from '@/migrations/2-actors-once-in-vehicle';
+
+export const enum MigrationStatus {
+	SUCCESS,
+	FAILURE,
+}
+
+const migrationScripts: Array<(migrationId: number) => Promise<MigrationStatus>> = [migrate_UseUuidForVehicles, migrate_ActorOnlyOnceInCrew];
+
+export async function performMigrations() {
+	const currentMigrationVersion = game.settings.get<number>(NAMESPACE, KEY_MIGRATION_VERSION) ?? 0;
+	const totalMigrationScripts = migrationScripts.length;
+	if (currentMigrationVersion === totalMigrationScripts) {
+		return;
+	}
+
+	const isGmHub = game.users.activeGM?.isSelf ?? (game.user.isGM && game.users.filter((user) => user.isGM && user.active).every((candidate) => candidate.id >= game.user.id));
+	if (!isGmHub) {
+		ui.notifications.error('Genesys.Migration.MustPerformMigration', { localize: true, permanent: true });
+		return;
+	}
+
+	let lastMigrationRun = currentMigrationVersion;
+	ui.notifications.warn(game.i18n.format('Genesys.Migration.StartingMigration', { amount: totalMigrationScripts - lastMigrationRun }), { permanent: true });
+
+	for (; lastMigrationRun < totalMigrationScripts; lastMigrationRun++) {
+		const migrationId = lastMigrationRun + 1;
+		ui.notifications.warn(game.i18n.format('Genesys.Migration.StartingScriptMigration', { id: migrationId }), { permanent: true });
+
+		let migrationScriptResponse: MigrationStatus;
+		try {
+			migrationScriptResponse = await migrationScripts[lastMigrationRun](migrationId);
+		} catch (error) {
+			migrationScriptResponse = MigrationStatus.FAILURE;
+			console.error(error);
+		}
+
+		if (migrationScriptResponse === MigrationStatus.FAILURE) {
+			ui.notifications.error(game.i18n.format('Genesys.Migration.StoppedScriptMigration', { id: migrationId }), { permanent: true });
+			break;
+		}
+
+		ui.notifications.warn(game.i18n.format('Genesys.Migration.CompletedScriptMigration', { id: migrationId }), { permanent: true });
+	}
+
+	const runAmountMigrations = lastMigrationRun - currentMigrationVersion;
+	if (runAmountMigrations > 0) {
+		await game.settings.set(NAMESPACE, KEY_MIGRATION_VERSION, lastMigrationRun);
+		ui.notifications.info('Genesys.Migration.MultipleSuccessfulMigration', { localize: true, permanent: true });
+		migrationScripts.splice(0, runAmountMigrations);
+	}
+
+	const remainingAmountMigrations = totalMigrationScripts - lastMigrationRun;
+	if (remainingAmountMigrations > 0) {
+		ui.notifications.error(game.i18n.format('Genesys.Migration.StoppedMigration', { amount: remainingAmountMigrations }), { permanent: true });
+	} else {
+		ui.notifications.info('Genesys.Migration.CompletedMigration', { localize: true, permanent: true });
+	}
+}
