188b7343478026f4bc9fd202a4cda5a7d66c50aa
Assembling Kings <assembling.kings@outlook.com>
2024-02-25 21:03:46 -0500

    Highlight drop areas when dragging from compendia

---
+++ src/sidebar/GenesysCompendium.ts
@@ -0,0 +1,16 @@
+import { DragTransferData, constructDragTransferTypeFromData } from '@/data/DragTransferData';
+
+export default class GenesysCompendium extends Compendium {
+	protected override _onDragStart(event: ElementDragEvent): void {
+		super._onDragStart(event);
+
+		const dragData = JSON.parse(event.dataTransfer?.getData('text/plain') ?? '{}') as DragTransferData;
+		if (dragData.uuid) {
+			const draggedEntity = fromUuidSync(dragData.uuid) as { type: string } | null;
+			if (draggedEntity) {
+				const genesysTransferType = constructDragTransferTypeFromData(draggedEntity.type, dragData.uuid as ItemUUID | ActorUUID);
+				event.dataTransfer?.setData(genesysTransferType, '');
+			}
+		}
+	}
+}
