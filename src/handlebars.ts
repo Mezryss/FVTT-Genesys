0142aad78b18f945be4115e65b6cc12c5c104023
Assembling Kings <assembling.kings@outlook.com>
2024-09-09 20:23:52 -0400

    More options to modify dice pool with effects

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -8,30 +8,31 @@
 
 export function register() {
 	/** String Utilities */
-	Handlebars.registerHelper('capitalize', /** @param {string} value */ (value) => value.capitalize());
-	Handlebars.registerHelper('toLowerCase', /** @param {string} value */ (value) => value.toLowerCase());
-	Handlebars.registerHelper('toUpperCase', /** @param {string} value */ (value) => value.toUpperCase());
-	Handlebars.registerHelper('concat', (...values) => values.filter((v) => v && typeof v === 'string').join(''));
-	Handlebars.registerHelper('split', /** @param {string} value */ (value) => value.split(' '));
+	Handlebars.registerHelper('capitalize', (value: string) => value.capitalize());
+	Handlebars.registerHelper('toLowerCase', (value: string) => value.toLowerCase());
+	Handlebars.registerHelper('toUpperCase', (value: string) => value.toUpperCase());
+	Handlebars.registerHelper('concat', (...values: any[]) => values.filter((v) => v && typeof v === 'string').join(''));
+	Handlebars.registerHelper('split', (value: string) => value.split(' '));
+	Handlebars.registerHelper('isSubstringOf', (substring: string, fullString: string) => fullString.includes(substring));
 
 	/** Math Utilities */
-	Handlebars.registerHelper('add', (lhs, rhs) => lhs + rhs);
-	Handlebars.registerHelper('sub', (lhs, rhs) => lhs - rhs);
-	Handlebars.registerHelper('mul', (lhs, rhs) => lhs * rhs);
-	Handlebars.registerHelper('div', (lhs, rhs) => lhs / rhs);
-	Handlebars.registerHelper('min', (lhs, rhs) => Math.min(lhs, rhs));
-	Handlebars.registerHelper('max', (lhs, rhs) => Math.max(lhs, rhs));
-	Handlebars.registerHelper('abs', (val) => Math.abs(val));
-	Handlebars.registerHelper('floor', (val) => Math.floor(val));
+	Handlebars.registerHelper('add', (lhs: number, rhs: number) => lhs + rhs);
+	Handlebars.registerHelper('sub', (lhs: number, rhs: number) => lhs - rhs);
+	Handlebars.registerHelper('mul', (lhs: number, rhs: number) => lhs * rhs);
+	Handlebars.registerHelper('div', (lhs: number, rhs: number) => lhs / rhs);
+	Handlebars.registerHelper('min', (lhs: number, rhs: number) => Math.min(lhs, rhs));
+	Handlebars.registerHelper('max', (lhs: number, rhs: number) => Math.max(lhs, rhs));
+	Handlebars.registerHelper('abs', (val: number) => Math.abs(val));
+	Handlebars.registerHelper('floor', (val: number) => Math.floor(val));
 	Handlebars.registerHelper('toFixed', (val: number, fractionDigits: number) => val.toFixed(fractionDigits));
 
 	/** Logic Utilities */
-	Handlebars.registerHelper('and', (lhs, rhs) => lhs && rhs);
-	Handlebars.registerHelper('or', (lhs, rhs) => lhs || rhs);
-	Handlebars.registerHelper('not', (val) => !val);
+	Handlebars.registerHelper('and', (lhs: boolean, rhs: boolean) => lhs && rhs);
+	Handlebars.registerHelper('or', (lhs: boolean, rhs: boolean) => lhs || rhs);
+	Handlebars.registerHelper('not', (val: boolean) => !val);
 
 	/** Iteration utilities */
-	Handlebars.registerHelper('repeat', (times, options) => {
+	Handlebars.registerHelper('repeat', (times: number, options: { fn: (time: number) => string | number }) => {
 		const results = [];
 
 		for (let i = 0; i < times; i++) {
